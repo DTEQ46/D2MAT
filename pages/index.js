850d7e6f177b06f78274fe20aa6397277979e959
DTEQ46 <87316415+DTEQ46@users.noreply.github.com>
2022-05-21 19:51:31 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { Grid, Typography } from '@material-ui/core';
+import Layout from '../components/Layout';
+import db from '../utils/db';
+import Product from '../models/Product';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { Store } from '../utils/Store';
+import ProductItem from '../components/ProductItem';
+
+export default function Home(props) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { topRatedProducts } = props;
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+  return (
+    <Layout>
+      <Typography variant="h2">Popular Products</Typography>
+      <Grid container spacing={3}>
+        {topRatedProducts.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <ProductItem
+              product={product}
+              addToCartHandler={addToCartHandler}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const topRatedProductsDocs = await Product.find({}, '-reviews')
+    .lean()
+    .sort({
+      rating: -1,
+    })
+    .limit(9);
+  await db.disconnect();
+  return {
+    props: {
+      topRatedProducts: topRatedProductsDocs.map(db.convertDocToObj),
+    },
+  };
+}
---
+++ pages/search.js
@@ -0,0 +1,171 @@
+import {
+  Box,
+  Button,
+  Grid,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { useRouter } from 'next/router';
+import React, { useContext } from 'react';
+import Layout from '../components/Layout';
+import db from '../utils/db';
+import Product from '../models/Product';
+import useStyles from '../utils/styles';
+import ProductItem from '../components/ProductItem';
+import { Store } from '../utils/Store';
+import axios from 'axios';
+
+import { Pagination } from '@material-ui/lab';
+
+const PAGE_SIZE = 3;
+
+export default function Search(props) {
+  const classes = useStyles();
+  const router = useRouter();
+  const { query = 'all', category = 'all' } = router.query;
+  const { products, countProducts, categories, pages } = props;
+
+  const filterSearch = ({
+    page,
+    category,
+
+    searchQuery,
+  }) => {
+    const path = router.pathname;
+    const { query } = router;
+    if (page) query.page = page;
+    if (searchQuery) query.searchQuery = searchQuery;
+    if (category) query.category = category;
+    router.push({
+      pathname: path,
+      query: query,
+    });
+  };
+  const categoryHandler = (e) => {
+    filterSearch({ category: e.target.value });
+  };
+  const pageHandler = (e, page) => {
+    filterSearch({ page });
+  };
+
+  const { state, dispatch } = useContext(Store);
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+  return (
+    <Layout title="Search">
+      <Grid className={classes.mt1} container spacing={1}>
+        <Grid item md={3}>
+          <List>
+            <ListItem>
+              <Box className={classes.fullWidth}>
+                <Typography>Categories</Typography>
+                <Select fullWidth value={category} onChange={categoryHandler}>
+                  <MenuItem value="all">All</MenuItem>
+                  {categories &&
+                    categories.map((category) => (
+                      <MenuItem key={category} value={category}>
+                        {category}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </Box>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={9}>
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Grid item>
+              {products.length === 0 ? 'No' : countProducts} Results
+              {query !== 'all' && query !== '' && ' : ' + query}
+              {category !== 'all' && ' : ' + category}
+              {(query !== 'all' && query !== '') || category !== 'all' ? (
+                <Button onClick={() => router.push('/search')}>
+                  <CancelIcon />
+                </Button>
+              ) : null}
+            </Grid>
+          </Grid>
+          <Grid className={classes.mt1} container spacing={3}>
+            {products.map((product) => (
+              <Grid item md={4} key={product.name}>
+                <ProductItem
+                  product={product}
+                  addToCartHandler={addToCartHandler}
+                />
+              </Grid>
+            ))}
+          </Grid>
+          <Pagination
+            className={classes.mt1}
+            defaultPage={parseInt(query.page || '1')}
+            count={pages}
+            onChange={pageHandler}
+          ></Pagination>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  await db.connect();
+  const pageSize = query.pageSize || PAGE_SIZE;
+  const page = query.page || 1;
+  const category = query.category || '';
+  const searchQuery = query.query || '';
+
+  const queryFilter =
+    searchQuery && searchQuery !== 'all'
+      ? {
+          name: {
+            $regex: searchQuery,
+            $options: 'i',
+          },
+        }
+      : {};
+  const categoryFilter = category && category !== 'all' ? { category } : {};
+
+  const categories = await Product.find().distinct('category');
+  const productDocs = await Product.find(
+    {
+      ...queryFilter,
+      ...categoryFilter,
+    },
+    '-reviews'
+  )
+
+    .skip(pageSize * (page - 1))
+    .limit(pageSize)
+    .lean();
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter,
+    ...categoryFilter,
+  });
+  await db.disconnect();
+
+  const products = productDocs.map(db.convertDocToObj);
+
+  return {
+    props: {
+      products,
+      countProducts,
+      page,
+      pages: Math.ceil(countProducts / pageSize),
+      categories,
+    },
+  };
+}
